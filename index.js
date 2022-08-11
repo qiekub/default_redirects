afa1ca50b326fc525176e7cf549abe4e89f0a7a9
thomasrosen <thomas.rosen@me.com>
2022-08-11 15:57:05 +0200

    feat: started redirects file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const port = 11692
+const host = '0.0.0.0'
+
+app.use(function (req, res, next) {
+  const host = req.get('host')
+  const origin = req.get('origin')
+  console.log('host:', host)
+  console.log('origin:', origin)
+
+  const domain = req.get('origin') || req.get('host') || ''
+  if (domain === '' || domain === 'localhost' || domain.startsWith('localhost:')) {
+    res.status(404).send('<h1>localhost<h1>')
+    return
+  }
+
+  res.status(404).send('<h1>redirect<h1>')
+})
+
+// app.get('/', (req, res) => {
+//   res.status(200).type('html').send('<h1>Hello World!<h1>')
+// })
+
+app.listen(port, host, () => {
+  console.info(`Express app listening at http://${host}:${port}`)
+})
