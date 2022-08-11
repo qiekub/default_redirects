d126ed37c38155b700d0f341728c09cbb71b7f46
thomasrosen <thomas.rosen@me.com>
2022-08-11 16:06:17 +0200

    fix: changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = 11692
+const port = 18418 // redirect > rdrct > rdr < 18418
 const host = '0.0.0.0'
 
 app.use(function (req, res, next) {
